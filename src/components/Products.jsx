7709211faf3e5e88e01bd8af4c77849cff717279
omeryasindemir <omeryasindemir66@gmail.com>
2024-03-23 14:09:49 +0300

    product

---
+++ src/components/Products.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <React.Fragment>
+      <div className="products_title">Popular Products</div>
+      <div className="product_wrapper">
+        <div className="product_card">
+          <div className="popular_icon">
+            <img
+              src="https://cdn.dsmcdn.com/mnresize/250/250/marketing/datascience/automation/2020/12/9/EnCokSatan_202012091129.png"
+              alt=""
+            />
+          </div>
+          <div className="product_image_wrapper">
+            <img src="https://bzgrup.com/img/m2.jpg" alt="" />
+          </div>
+          <div className="product_banner">Best Quality!</div>
+          <div className="product_des">
+            <span>Title</span> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Aspernatur, fugiat!
+          </div>
+          <div className="product_star">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-half"></i>
+          </div>
+          <div className="product_btn_wrapper">
+            <button>See More!</button>
+          </div>
+        </div>
+        <div className="product_card">
+          <div className="popular_icon">
+            <img
+              src="https://cdn.dsmcdn.com/mnresize/250/250/marketing/datascience/automation/2020/12/9/EnCokSatan_202012091129.png"
+              alt=""
+            />
+          </div>
+          <div className="product_image_wrapper">
+            <img src="https://bzgrup.com/img/m2.jpg" alt="" />
+          </div>
+          <div className="product_banner">Best Quality!</div>
+          <div className="product_des">
+            <span>Title</span> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Aspernatur, fugiat!
+          </div>
+          <div className="product_star">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-half"></i>
+          </div>
+          <div className="product_btn_wrapper">
+            <button>See More!</button>
+          </div>
+        </div>
+        <div className="product_card">
+          <div className="popular_icon">
+            <img
+              src="https://cdn.dsmcdn.com/mnresize/250/250/marketing/datascience/automation/2020/12/9/EnCokSatan_202012091129.png"
+              alt=""
+            />
+          </div>
+          <div className="product_image_wrapper">
+            <img src="https://bzgrup.com/img/m2.jpg" alt="" />
+          </div>
+          <div className="product_banner">Best Quality!</div>
+          <div className="product_des">
+            <span>Title</span> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Aspernatur, fugiat!
+          </div>
+          <div className="product_star">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-half"></i>
+          </div>
+          <div className="product_btn_wrapper">
+            <button>See More!</button>
+          </div>
+        </div>
+        <div className="product_card">
+          <div className="popular_icon">
+            <img
+              src="https://cdn.dsmcdn.com/mnresize/250/250/marketing/datascience/automation/2020/12/9/EnCokSatan_202012091129.png"
+              alt=""
+            />
+          </div>
+          <div className="product_image_wrapper">
+            <img src="https://bzgrup.com/img/m2.jpg" alt="" />
+          </div>
+          <div className="product_banner">Best Quality!</div>
+          <div className="product_des">
+            <span>Title</span> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Aspernatur, fugiat!
+          </div>
+          <div className="product_star">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-half"></i>
+          </div>
+          <div className="product_btn_wrapper">
+            <button>See More!</button>
+          </div>
+        </div>
+        <div className="product_card">
+          <div className="popular_icon">
+            <img
+              src="https://cdn.dsmcdn.com/mnresize/250/250/marketing/datascience/automation/2020/12/9/EnCokSatan_202012091129.png"
+              alt=""
+            />
+          </div>
+          <div className="product_image_wrapper">
+            <img src="https://bzgrup.com/img/m2.jpg" alt="" />
+          </div>
+          <div className="product_banner">Best Quality!</div>
+          <div className="product_des">
+            <span>Title</span> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Aspernatur, fugiat!
+          </div>
+          <div className="product_star">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-half"></i>
+          </div>
+          <div className="product_btn_wrapper">
+            <button>See More!</button>
+          </div>
+        </div>
+        <div className="product_card">
+          <div className="popular_icon">
+            <img
+              src="https://cdn.dsmcdn.com/mnresize/250/250/marketing/datascience/automation/2020/12/9/EnCokSatan_202012091129.png"
+              alt=""
+            />
+          </div>
+          <div className="product_image_wrapper">
+            <img src="https://bzgrup.com/img/m2.jpg" alt="" />
+          </div>
+          <div className="product_banner">Best Quality!</div>
+          <div className="product_des">
+            <span>Title</span> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Aspernatur, fugiat!
+          </div>
+          <div className="product_star">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-half"></i>
+          </div>
+          <div className="product_btn_wrapper">
+            <button>See More!</button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Products;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Slider from '../components/Slider'
+import Products from '../components/Products'
 
 const HomePage = () => {
   return (
     <React.Fragment>
         <Header/>
         <Slider/>
+        <Products/>
     </React.Fragment>
   )
 }
